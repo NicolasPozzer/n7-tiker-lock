e2d5011cdd9308c6c58f9b51d380c3a3add9ea27
NicolasPozzer <pozzernico@gmail.com>
2026-02-17 22:19:44 -0300

    Add: log de ejecucion en la interfaz

--- src/main/java/org/example/controller/Logica.java
+++ src/main/java/org/example/controller/Logica.java
@@ -2,11 +2,9 @@ package org.example.controller;
 
 import javax.swing.*;
 
-public class Logica {
-
+public class Logica extends JFrame {
 
     public void cargarTickers() {
 
     }
-
 }
--- src/main/java/org/example/gui/Principal.java
+++ src/main/java/org/example/gui/Principal.java
@@ -3,44 +3,63 @@ package org.example.gui;
 import org.example.model.Tiker;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Principal extends JFrame {
     public JPanel panel;
     public JPanel panel_con_botones;
+    public JTextArea textAreaLogs;
+    private JButton btnReload;
+    private JPanel panel_logs;
+    private JScrollPane scrollLogs;
 
-    // main del JFrame
     public Principal() {
+
         listarBotones();
+
+        btnReload.addActionListener(e -> {
+            log("🔄 Recargando lista...");
+            listarBotones();
+        });
     }
 
-
     public void listarBotones(){
+
+        panel_con_botones.removeAll(); // limpiar antes de recargar
+
         List<Tiker> tikers = new ArrayList<>();
 
-        Tiker tic = new Tiker(1,"BTC/USDT:","BTC",true);
-        Tiker tic2 = new Tiker(2,"BNB/USDT:","BNB",true);
-        Tiker tic3 = new Tiker(3,"ETH/USDT:","ETH",true);
+        tikers.add(new Tiker(1,"BTC/USDT:","BTC",true));
+        tikers.add(new Tiker(2,"BNB/USDT:","BNB",true));
+        tikers.add(new Tiker(3,"ETH/USDT:","ETH",true));
 
-        tikers.add(tic);
-        tikers.add(tic2);
-        tikers.add(tic3);
+        log("Cargando botones...");
 
         for (Tiker tiker : tikers) {
-            System.out.println("Cargando Botones......");
             JButton boton = new JButton(tiker.getNombre());
 
             boton.addActionListener(e -> {
-                System.out.println("Presionaste: " + tiker.getNombre());
+                log("Presionaste: " + tiker.getNombre());
             });
 
             panel_con_botones.add(boton);
         }
 
+        panel_con_botones.revalidate();
+        panel_con_botones.repaint();
+
+        log("Botones cargados correctamente.");
+    }
+
+
+    //log
+    public void log(String mensaje) {
+        SwingUtilities.invokeLater(() -> {
+            textAreaLogs.append(mensaje + "\n");
+            textAreaLogs.setCaretPosition(textAreaLogs.getDocument().getLength());
+        });
     }
 }
-
-
-
-
